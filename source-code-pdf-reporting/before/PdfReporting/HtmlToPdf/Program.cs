a22dc32eae9f1ca6bc08d44d37bb507fd891e8f1
jmoralesv <korum03@gmail.com>
2025-10-05 21:30:41 -0500

    Adding the source code of a PDF templating solution that uses Handlebars.Net and PuppeteerSharp for PDF rendering.

---
+++ source-code-pdf-reporting/before/PdfReporting/HtmlToPdf/Program.cs
@@ -0,0 +1,20 @@
+using HtmlToPdf;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<InvoiceFactory>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
